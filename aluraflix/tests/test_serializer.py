500db6f3b9ba164dd1064cebba4a1384b5d091ac
bonetou <h.boneto@outlook.com>
2021-06-14 15:34:28 -0300

    teste do serializer

--- aluraflix/tests/test_serializer.py
+++ aluraflix/tests/test_serializer.py
@@ -16,6 +16,15 @@ class ProgramaSerializerTestCase(TestCase):
         self.serializer = ProgramaSerializer(instance=self.programa)
     
     def test_verifica_campos_serializados(self):
-        """Teste verificador que verifica os campos que estão sendo serializados"""
+        """Teste que verifica os campos que estão sendo serializados"""
         data = self.serializer.data
         self.assertEqual(set(data.keys()), set(['titulo', 'tipo', 'data_lancamento', 'likes'])) 
+
+    def test_verifica_conteudo_dos_campos_serializados(self):
+        """Teste que verifica o conteúdo dos campos serializados"""
+        data = self.serializer.data
+        self.assertEqual(data['titulo'], self.programa.titulo)
+        self.assertEqual(data['data_lancamento'], self.programa.data_lancamento)
+        self.assertEqual(data['tipo'], self.programa.tipo)
+        self.assertEqual(data['likes'], self.programa.likes)
+    
